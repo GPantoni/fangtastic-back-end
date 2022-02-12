42561d699f1ee282c9f6732f02ed7ae69fb3cff9
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2022-02-12 15:39:20 -0300

    feat/product_screen

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { searchProducts } from "../controllers/productsController.js";
+import { searchProducts, dataProduct } from "../controllers/productsController.js";
 
 const productsRouter = Router();
 
 productsRouter.get("/products", searchProducts);
+productsRouter.get("/product", dataProduct);
 
 export default productsRouter;

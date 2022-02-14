d904faa3c877def8690806f3a0d369ed56def447
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-02-14 17:08:32 -0300

    fix

--- src/routes/checkoutRouter.js
+++ src/routes/checkoutRouter.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { getFormsOfPayment, addFormOfPayment, removeFormOfPayment, sendOrder } from "../controllers/checkoutController.js";
-import { validateformOfPaymentSchema } from "../middleware/validateFormOfPaymentSchema.js";
 
 const checkoutRouter = Router();
 
 checkoutRouter.get("/checkout", getFormsOfPayment);
-checkoutRouter.post("/checkout", addFormOfPayment, validateformOfPaymentSchema);
+checkoutRouter.post("/checkout", addFormOfPayment);
 checkoutRouter.post("/order", sendOrder);
 checkoutRouter.delete('/checkout', removeFormOfPayment)
 
